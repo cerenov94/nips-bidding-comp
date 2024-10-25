d161f2ecc2c198b246c6e2cf3b79d6d69183fea7
cerenov94 <cerenov1994@gmail.com>
2024-10-25 17:37:52 +0300

    u

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-docker build -t registry-intl.cn-beijing.aliyuncs.com/niptest/niprep:sub193 -f ./Dockerfile .
-docker push registry-intl.cn-beijing.aliyuncs.com/niptest/niprep:sub193
+docker build -t registry-intl.cn-beijing.aliyuncs.com/niptest/niprep:sub207 -f ./Dockerfile .
+docker push registry-intl.cn-beijing.aliyuncs.com/niptest/niprep:sub207
 
+# sub203 step:19800,q_value:12.897,12.898,q_loss:0.312,0.213.pth score 0.39
